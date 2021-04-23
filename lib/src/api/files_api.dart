4ad095ffcc44167e9276ed927190ddb94a18b0ca
Markus Krebs <markuskrebs93@gmail.com>
2021-04-23 23:00:28 +0200

    added files api

---
+++ lib/src/api/files_api.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+import '../../dart_backend_client.dart';
+
+part 'files_api.g.dart';
+
+@RestApi(autoCastResponse: true)
+abstract class FilesRestClient {
+  factory FilesRestClient(Dio dio, {String? baseUrl}) = _FilesRestClient;
+
+  @GET("/files/{fileName}")
+  @DioResponseType(ResponseType.plain)
+  Future<String> downloadFile(@Path("fileName") String fileName);
+
+  @GET("/images/{fileName}")
+  @DioResponseType(ResponseType.bytes)
+  Future<List<int>> downloadImageFile(@Path("fileName") String fileName);
+}
---
+++ lib/src/api/files_api.g.dart
@@ -0,0 +1,63 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'files_api.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+class _FilesRestClient implements FilesRestClient {
+  _FilesRestClient(this._dio, {this.baseUrl});
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<String> downloadFile(fileName) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result = await _dio.fetch<String>(_setStreamType<String>(Options(
+            method: 'GET',
+            headers: <String, dynamic>{},
+            extra: _extra,
+            responseType: ResponseType.plain)
+        .compose(_dio.options, '/files/$fileName',
+            queryParameters: queryParameters, data: _data)
+        .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = _result.data!;
+    return value;
+  }
+
+  @override
+  Future<List<int>> downloadImageFile(fileName) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result = await _dio.fetch<List<dynamic>>(_setStreamType<List<int>>(
+        Options(
+                method: 'GET',
+                headers: <String, dynamic>{},
+                extra: _extra,
+                responseType: ResponseType.bytes)
+            .compose(_dio.options, '/images/$fileName',
+                queryParameters: queryParameters, data: _data)
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = _result.data!.cast<int>();
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+}
