97bb7e18bfbdef84df1e102d878914d2a5e0dda2
Markus Krebs <markuskrebs93@gmail.com>
2021-07-31 17:09:03 +0200

    added dio mock test

--- test/init_test.dart
+++ test/init_test.dart
@@ -6,5 +6,12 @@ void main() {
     test('Init', () async {
       initModels();
     });
+    test('MockDio Test', () async {
+      final obj = ImageObject();
+      final MockDio dio = MockDio()..data = <ImageObject>[obj];
+      final val = await dio.get('');
+      expect(val, isNotNull);
+      expect(val.data, obj);
+    });
   });
 }
