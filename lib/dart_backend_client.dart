b026b8a03f38243dc6e6c4c82e6abae638609894
Markus <markuskrebs93@gmail.com>
2021-11-16 12:07:12 +0100

    fix: import and export of library

--- lib/dart_backend_client.dart
+++ lib/dart_backend_client.dart
@@ -3,20 +3,6 @@ library dart_backend_client;
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:dart_backend_client/src/api/credits_api.dart';
-import 'package:dart_backend_client/src/api/events_api.dart';
-import 'package:dart_backend_client/src/api/extended_user_api.dart';
-import 'package:dart_backend_client/src/api/feedbacks_api.dart';
-import 'package:dart_backend_client/src/api/image_api.dart';
-import 'package:dart_backend_client/src/api/locations_api.dart';
-import 'package:dart_backend_client/src/api/messages_api.dart';
-import 'package:dart_backend_client/src/api/news_api.dart';
-import 'package:dart_backend_client/src/api/newsletters_api.dart';
-import 'package:dart_backend_client/src/api/registers_api.dart';
-import 'package:dart_backend_client/src/api/todos_api.dart';
-import 'package:dart_backend_client/src/api/trainers_api.dart';
-import 'package:dart_backend_client/src/api/training_days_api.dart';
-import 'package:dart_backend_client/src/api/users_api.dart';
 import 'package:dio/dio.dart';
 import 'package:dio/native_imp.dart';
 import 'package:hive/hive.dart';
@@ -26,10 +12,25 @@ import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:retrofit/http.dart';
 
 import 'src/api/configs_api.dart';
+import 'src/api/credits_api.dart';
+import 'src/api/events_api.dart';
+import 'src/api/extended_user_api.dart';
+import 'src/api/feedbacks_api.dart';
+import 'src/api/image_api.dart';
+import 'src/api/locations_api.dart';
+import 'src/api/messages_api.dart';
+import 'src/api/news_api.dart';
+import 'src/api/newsletters_api.dart';
+import 'src/api/registers_api.dart';
+import 'src/api/todos_api.dart';
+import 'src/api/trainers_api.dart';
+import 'src/api/training_days_api.dart';
 import 'src/api/trainings_api.dart';
+import 'src/api/users_api.dart';
 
 export 'package:dio/dio.dart';
 export 'package:hive/hive.dart';
+export 'package:intl/intl.dart';
 
 export 'src/api/configs_api.dart';
 export 'src/api/credits_api.dart';
@@ -67,30 +68,28 @@ Future<void> initLocales(List<String> locales) async {
   }
 }
 
-void initModels() {
-  // initialize datetime format
-  Hive
-    ..registerAdapter(ConfigObjectAdapter())
-    ..registerAdapter(CreditObjectAdapter())
-    ..registerAdapter(EventObjectAdapter())
-    ..registerAdapter(FeedbackObjectAdapter())
-    ..registerAdapter(ImageObjectAdapter())
-    ..registerAdapter(LocationObjectAdapter())
-    ..registerAdapter(MessageObjectAdapter())
-    ..registerAdapter(NewsObjectAdapter())
-    ..registerAdapter(NewsletterObjectAdapter())
-    ..registerAdapter(RegisterObjectAdapter())
-    ..registerAdapter(TodoObjectAdapter())
-    ..registerAdapter(TrainerObjectAdapter())
-    ..registerAdapter(TrainingDateObjectAdapter())
-    ..registerAdapter(TrainingsObjectAdapter())
-    ..registerAdapter(UserObjectAdapter())
-    ..registerAdapter(PushTokenObjectAdapter())
-    ..registerAdapter(UserTrainingDateObjectAdapter())
-    ..registerAdapter(UserEventObjectAdapter())
-    ..registerAdapter(UserTrainingObjectAdapter())
-    ..registerAdapter(ExtendedDataAdapter());
-}
+// initialize datetime format
+void initModels() => Hive
+  ..registerAdapter(ConfigObjectAdapter())
+  ..registerAdapter(CreditObjectAdapter())
+  ..registerAdapter(EventObjectAdapter())
+  ..registerAdapter(FeedbackObjectAdapter())
+  ..registerAdapter(ImageObjectAdapter())
+  ..registerAdapter(LocationObjectAdapter())
+  ..registerAdapter(MessageObjectAdapter())
+  ..registerAdapter(NewsObjectAdapter())
+  ..registerAdapter(NewsletterObjectAdapter())
+  ..registerAdapter(RegisterObjectAdapter())
+  ..registerAdapter(TodoObjectAdapter())
+  ..registerAdapter(TrainerObjectAdapter())
+  ..registerAdapter(TrainingDateObjectAdapter())
+  ..registerAdapter(TrainingsObjectAdapter())
+  ..registerAdapter(UserObjectAdapter())
+  ..registerAdapter(PushTokenObjectAdapter())
+  ..registerAdapter(UserTrainingDateObjectAdapter())
+  ..registerAdapter(UserEventObjectAdapter())
+  ..registerAdapter(UserTrainingObjectAdapter())
+  ..registerAdapter(ExtendedDataAdapter());
 
 abstract class StorageRepository<T> {
   void safe(T data);
