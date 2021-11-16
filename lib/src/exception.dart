c71bd8e9a05cf8634e41afc18efb6166e90c708e
Markus <markuskrebs93@gmail.com>
2021-11-16 12:25:17 +0100

    fix: nullability with dynamic type

--- lib/src/exception.dart
+++ lib/src/exception.dart
@@ -3,7 +3,7 @@ part of dart_backend_client;
 class BackendClientException implements Exception {
   final String message;
   final int? code;
-  final dynamic? response;
+  final dynamic response;
 
   const BackendClientException([this.message = "", this.code, this.response]);
 }
